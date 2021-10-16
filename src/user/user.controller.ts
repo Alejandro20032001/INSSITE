c0c17411baab095276fe184d073efb30ddc75579
Alejandro20032001 <dilanantezama@gmail.com>
2021-10-16 11:11:45 -0400

    servicios y rutas

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, NotFoundException, Param, Post, Res } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService){}
+    @Post()
+    async createUser(@Res() res, @Body() body:CreateUserDto){
+        const userCreated = await this.userService.createUser(body);
+        return res.status(HttpStatus.OK).json({
+            massage:'created',
+            userCreated,
+        });
+    }
+    @Get('/:idUser')
+    async getUser(@Res() res, @Param('idUser') idUser: string) {
+        const userFounded = await this.userService.getOneUser(idUser);
+        if (!userFounded) throw new NotFoundException('Not exist');
+        return res.status(HttpStatus.OK).json(userFounded);
+    }
+    @Get('')
+    async getAllUsers(@Res() res){
+        const usersFounded = await this.userService.getAllUsers();
+        return res.status(HttpStatus.OK).json(usersFounded)
+    }
+    @Get('/students')
+    async getAllStudent(@Res() res){
+        const studentsFounded = await this.userService.getAllStudents();
+        return res.status(HttpStatus.OK).json(studentsFounded)
+    }
+    @Get('/teachers')
+    async getAllTeachers(@Res() res){
+        const teachersFounded = await this.userService.getAllTeachers();
+        return res.status(HttpStatus.OK).json(teachersFounded);
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserEntity } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository:Repository<UserEntity>,
+    ){}
+    async createUser(createUserDto: CreateUserDto): Promise<UserEntity>{
+        const userCreated = this.userRepository.create(createUserDto);
+        return await this.userRepository.save(userCreated);
+    }
+    async getOneUser(idUser: string): Promise<UserEntity> {
+        return await this.userRepository.findOne(idUser);
+    }
+    async getAllUsers(): Promise<UserEntity[]>{
+        return await this.userRepository.find({});
+    }
+    async getAllStudents(): Promise<UserEntity[]>{
+        return await this.userRepository.find({ where: {userRole: 'ESTUDIANTE' }});
+    }
+    async getAllTeachers(): Promise<UserEntity[]>{
+        return await this.userRepository.find({ where:{userRole: 'DOCENTE'}});
+    }
+}
