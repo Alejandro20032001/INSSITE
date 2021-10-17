d33275c2ff1d9f0133c0ce50527151ae51d7a5a3
Alejandro20032001 <dilanantezama@gmail.com>
2021-10-17 11:08:01 -0400

    importtaciones necesarias

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from 'src/user/user.module';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    PassportModule,
+    UserModule
+  ],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
