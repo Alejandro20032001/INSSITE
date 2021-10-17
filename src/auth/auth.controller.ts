fc1dbbd5b900f79428c049b00bb5f8ef2ce32e9f
Alejandro20032001 <dilanantezama@gmail.com>
2021-10-17 14:59:27 -0400

    controler auth con validacion de usuario

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { User } from 'src/common/decorators/user.decorator';
+import { UserEntity } from 'src/user/user.entity';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { LocalAuthGuard } from './guards';
 
+@ApiTags('Auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService){}
+    
+    @UseGuards(LocalAuthGuard)
+    @Post('login')
+    async login(@Body() logindto: LoginDto, @User() user: UserEntity){
+        const data = await this.authService.login(user);
+        return {
+            massage: 'Successfull login',
+            data
+        };
+    }
+}   
