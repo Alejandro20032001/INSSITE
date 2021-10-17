e5af4e317a3383ce27076d0d2b63afad5ad3673b
Alejandro20032001 <dilanantezama@gmail.com>
2021-10-17 14:55:14 -0400

    validacion de username con password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcryptjs';
+import { UserEntity } from 'src/user/user.entity';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly userService: UserService,
+    ){}
+
+    async validateUser(username:string, password :string){
+       const user =  await this.userService.findeByUserName({ username }); 
+       if(user && await ( compare(password, user.password) ) ){
+           const { password, ...rest } = user;
+        return rest;
+       }
+       return null;
+    }
+
+}
