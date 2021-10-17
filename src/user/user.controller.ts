9eaed2a8cd75cfbc24cece7f27e31c6f39d8375d
Alejandro20032001 <dilanantezama@gmail.com>
2021-10-17 10:33:27 -0400

    swagger documentation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, HttpStatus, NotFoundException, Param, Post, Res } from '@nestjs/common';
+import { ApiBody, ApiCreatedResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
-
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService){}
@@ -13,25 +13,30 @@ export class UserController {
             userCreated,
         });
     }
+
+    @Get('/students')
+    async getAllStudent(@Res() res){
+        const studentsFounded = await this.userService.getAllStudents();
+        return res.status(HttpStatus.OK).json(studentsFounded)
+    }
+
+    @Get('/teachers')
+    async getAllTeachers(@Res() res){
+        const teachersFounded = await this.userService.getAllTeachers();
+        console.log(teachersFounded);
+        return res.status(HttpStatus.OK).json(teachersFounded);
+    }
+
     @Get('/:idUser')
     async getUser(@Res() res, @Param('idUser') idUser: string) {
         const userFounded = await this.userService.getOneUser(idUser);
         if (!userFounded) throw new NotFoundException('Not exist');
         return res.status(HttpStatus.OK).json(userFounded);
     }
+
     @Get('')
     async getAllUsers(@Res() res){
         const usersFounded = await this.userService.getAllUsers();
         return res.status(HttpStatus.OK).json(usersFounded)
-    }
-    @Get('/students')
-    async getAllStudent(@Res() res){
-        const studentsFounded = await this.userService.getAllStudents();
-        return res.status(HttpStatus.OK).json(studentsFounded)
-    }
-    @Get('/teachers')
-    async getAllTeachers(@Res() res){
-        const teachersFounded = await this.userService.getAllTeachers();
-        return res.status(HttpStatus.OK).json(teachersFounded);
     }
 }
