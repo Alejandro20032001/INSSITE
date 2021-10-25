c5cb58bb3c919b48a5e635c368c37efd678a40ad
Alejandro20032001 <dilanantezama@gmail.com>
2021-10-25 10:44:55 -0400

    base de datos local

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,33 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { CourseModule } from './course/course.module';
+import { AccessControlModule } from 'nest-access-control';
+import { roles } from './app.roles';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      /*type: 'mysql',
       host: 'us-cdbr-east-04.cleardb.com',
       port: 3306,
       username: 'bb0d82da493adf',
       password: 'c10e444f',
       database: 'heroku_42e4360e936cb76',
+      entities: [__dirname + './**//*entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+      */
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'inssite-local',
       entities: [__dirname + './**/**/*entity{.ts,.js}'],
       autoLoadEntities: true,
       synchronize: true,
     }),
+    AccessControlModule.forRoles(roles),
     UserModule,
     AuthModule,
     CourseModule,
