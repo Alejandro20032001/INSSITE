4c24aec946b59fc662725feceda2debe9e7659c1
Alejandro20032001 <dilanantezama@gmail.com>
2021-11-06 12:44:36 -0400

    refactorizacion en los auth

--- src/common/auth.decorator.ts
+++ src/common/auth.decorator.ts
@@ -1,7 +1,12 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { ACGuard, Role, UseRoles } from 'nest-access-control';
 import { JwtAuthGuards } from 'src/auth/guards';
 
-export function Auth() {
-  return applyDecorators(UseGuards(JwtAuthGuards), ApiBearerAuth());
+export function Auth(...roles: Role[]) {
+  return applyDecorators(
+    UseGuards(JwtAuthGuards, ACGuard),
+    UseRoles(...roles),
+    ApiBearerAuth(),
+  );
 }
