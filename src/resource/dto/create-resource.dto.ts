ad4873bb2470de2fa899edf50469b8285731538c
pabloskick <darkour.m@gmail.com>
2021-11-13 18:58:59 -0400

    creacion aarchivos dto y entity

---
+++ src/resource/dto/create-resource.dto.ts
@@ -0,0 +1,8 @@
+import { CourseModuleEntity } from '../../coursemodule/coursemodule.entity';
+
+export class CreateResourceDto {
+  title: string;
+  descriptionResource: string;
+  content: string;
+  module: CourseModuleEntity;
+}
