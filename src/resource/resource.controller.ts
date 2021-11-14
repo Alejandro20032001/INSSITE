cc2a3e5ab7db50ff67c484c08c1e03850cb6e9f8
Alejandro20032001 <dilanantezama@gmail.com>
2021-11-14 17:02:03 -0400

    solo firma post

--- src/resource/resource.controller.ts
+++ src/resource/resource.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { CreateResourceDto } from './dto/create-resource.dto';
+import { ResourceService } from './resource.service';
 
 @Controller('resource')
-export class ResourceController {}
+export class ResourceController {
+  constructor(private resourceService: ResourceService) {}
+  @Post()
+  async createResource(@Res() res, @Body() body: CreateResourceDto){
+  }
+}
