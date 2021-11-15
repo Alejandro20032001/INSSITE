761db1c92c629a99715a52d7b2b6c13689cc1322
Alejandro20032001 <dilanantezama@gmail.com>
2021-11-15 15:50:18 -0400

    restricciones en los modulos y recursos

--- src/app.roles.ts
+++ src/app.roles.ts
@@ -4,16 +4,20 @@ import { UserEnum } from './user/enum/user.enum';
 export enum AppResources {
   USER = 'USER',
   COURSE = 'COURSE',
+  MODULE = 'MODULE',
+  RESOURCE = 'RESOURCE',
 }
 export const roles: RolesBuilder = new RolesBuilder();
 
 roles
   .grant(UserEnum.DOCENTE)
-  .createOwn([AppResources.COURSE])
-  .readOwn([AppResources.COURSE])
+  .createOwn([AppResources.COURSE, AppResources.RESOURCE])
+  .readOwn([AppResources.COURSE, AppResources.MODULE, AppResources.RESOURCE])
   .updateOwn([AppResources.COURSE])
   .readAny([AppResources.USER])
+  .readOwn([AppResources.MODULE, AppResources.RESOURCE])
+  .deleteOwn([AppResources.MODULE, AppResources.RESOURCE])
 
   .grant(UserEnum.ESTUDIANTE)
-  .readAny([AppResources.COURSE])
+  .readAny([AppResources.COURSE, AppResources.MODULE, AppResources.RESOURCE])
   .updateAny([AppResources.COURSE]);
