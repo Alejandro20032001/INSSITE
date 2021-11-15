368a218dd72c7a14f7eee5b2ddd82b32c2d560a1
Alejandro20032001 <dilanantezama@gmail.com>
2021-11-15 15:53:18 -0400

    declaracion entidad recursos

--- src/resource/resource.module.ts
+++ src/resource/resource.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResourceController } from './resource.controller';
+import { ResourceEntity } from './resource.entity';
 import { ResourceService } from './resource.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ResourceEntity])],
   controllers: [ResourceController],
   providers: [ResourceService],
 })
