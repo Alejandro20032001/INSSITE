91b868874c0736720fe3cfb1a36954bf52565a6f
Alejandro20032001 <dilanantezama@gmail.com>
2021-11-15 15:52:10 -0400

    controller y services de recursos

--- src/resource/resource.controller.ts
+++ src/resource/resource.controller.ts
@@ -1,10 +1,86 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { InjectRolesBuilder, RolesBuilder } from 'nest-access-control';
+import { AppResources } from 'src/app.roles';
+import { Auth, User } from 'src/common';
+import { UserEntity } from 'src/user/user.entity';
 import { CreateResourceDto } from './dto/create-resource.dto';
 import { ResourceService } from './resource.service';
 
+@ApiTags('Resource')
 @Controller('resource')
 export class ResourceController {
-  constructor(private resourceService: ResourceService) {}
-  @Post()
-  async createResource(@Res() res, @Body() body: CreateResourceDto) {}
+  constructor(
+    private resourceService: ResourceService,
+    @InjectRolesBuilder()
+    private readonly rolesBuilder: RolesBuilder,
+  ) {}
+  @Auth({
+    possession: 'own',
+    action: 'create',
+    resource: AppResources.RESOURCE,
+  })
+  @Post('')
+  async createResource(
+    @Res() res,
+    @Body() body: CreateResourceDto,
+    @User() user: UserEntity,
+  ) {
+    if (
+      this.rolesBuilder.can(user.roles).createOwn(AppResources.RESOURCE).granted
+    ) {
+      const resourceCreated = await this.resourceService.createResource(body);
+      return res.status(HttpStatus.OK).json({
+        message: 'created',
+        resourceCreated,
+      });
+    } else
+      return res
+        .status(HttpStatus.FORBIDDEN)
+        .json({ message: 'This is not your resource' });
+  }
+  @Get('/:idResource')
+  async getOneResource(@Res() res, @Param('idResource') idResource: string) {
+    const resourceFounded = await this.resourceService.getOneResource(
+      idResource,
+    );
+    if (!resourceFounded) throw new NotFoundException('Not exist');
+    return res.status(HttpStatus.OK).json(resourceFounded);
+  }
+  @Auth({
+    possession: 'own',
+    action: 'delete',
+    resource: AppResources.RESOURCE,
+  })
+  @Delete('/:idResource')
+  async deleteResource(
+    @Res() res,
+    @Param('idResource') idResource: string,
+    @User() user: UserEntity,
+  ) {
+    if (
+      this.rolesBuilder.can(user.roles).deleteOwn(AppResources.RESOURCE).granted
+    ) {
+      const resourceDeleted = await this.resourceService.deleteResources(
+        idResource,
+      );
+      return res.status(HttpStatus.OK).json({
+        message: 'deleted',
+        resourceDeleted,
+      });
+    } else
+      return res
+        .status(HttpStatus.FORBIDDEN)
+        .json({ message: 'This is not your resource' });
+  }
 }
--- src/resource/resource.service.ts
+++ src/resource/resource.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateResourceDto } from './dto/create-resource.dto';
+import { ResourceEntity } from './resource.entity';
 
 @Injectable()
-export class ResourceService {}
+export class ResourceService {
+  constructor(
+    @InjectRepository(ResourceEntity)
+    private readonly resourceRepository: Repository<ResourceEntity>,
+  ) {}
+  async createResource(
+    createResourceDto: CreateResourceDto,
+  ): Promise<ResourceEntity> {
+    const resourceCreated = await this.resourceRepository.create(
+      createResourceDto,
+    );
+    return await this.resourceRepository.save(resourceCreated);
+  }
+  async deleteResources(idResource: string) {
+    return await this.resourceRepository.delete(idResource);
+  }
+  async getOneResource(idResource: string) {
+    const resourceFounded = await this.resourceRepository.findOne(idResource);
+    delete resourceFounded.idResource;
+    return resourceFounded;
+  }
+}
