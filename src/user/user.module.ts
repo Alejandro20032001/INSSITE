673cb485f9777e3807499d4bf23ce7ee6ebfb17f
Alejandro20032001 <dilanantezama@gmail.com>
2021-12-03 22:09:48 -0400

    importacion de servicios necesarios

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseModule } from 'src/course/course.module';
+import { CoursemoduleModule } from 'src/coursemodule/coursemodule.module';
 import { UserController } from './user.controller';
 import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity]), CourseModule],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    CourseModule,
+    CoursemoduleModule,
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
