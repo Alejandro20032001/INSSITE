eeb2320ca68aaf439b6bb35b4ece4e9c526ccf48
Alejandro20032001 <dilanantezama@gmail.com>
2021-12-03 22:05:02 -0400

    correccion en el modulo de homework

--- src/homework/homework.module.ts
+++ src/homework/homework.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { HomeworkController } from './homework.controller';
+import { HomeworkEntity } from './homework.entity';
 import { HomeworkService } from './homework.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([HomeworkEntity])],
   controllers: [HomeworkController],
   providers: [HomeworkService],
 })
