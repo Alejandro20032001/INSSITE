11fdbb13f83b89c2df47b9b6cf129300d5017822
pabloskick <darkour.m@gmail.com>
2021-12-10 13:40:13 -0400

    "

--- src/coursemodule/coursemodule.module.ts
+++ src/coursemodule/coursemodule.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoursemoduleController } from './coursemodule.controller';
 import { CoursemoduleService } from './coursemodule.service';
+import { ResourceModule } from 'src/resource/resource.module';
 import { CourseModuleEntity } from './coursemodule.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CourseModuleEntity])],
+  imports: [TypeOrmModule.forFeature([CourseModuleEntity]), ResourceModule],
   controllers: [CoursemoduleController],
   providers: [CoursemoduleService],
   exports: [CoursemoduleService],
