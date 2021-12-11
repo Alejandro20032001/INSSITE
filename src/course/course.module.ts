902247ec1e16d9164d672152204642b39829f8a2
Alejandro20032001 <dilanantezama@gmail.com>
2021-12-11 00:14:25 -0400

    importaciones necesarias

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseController } from './course.controller';
 import { CourseEntity } from './course.entity';
 import { CourseService } from './course.service';
+import { CoursemoduleModule } from '../coursemodule/coursemodule.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CourseEntity])],
+  imports: [TypeOrmModule.forFeature([CourseEntity]), CoursemoduleModule],
   controllers: [CourseController],
   providers: [CourseService],
   exports: [CourseService],
